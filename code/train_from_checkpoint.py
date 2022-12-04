7ca140fa2cc959b4f27480aa76b7d95ef1fab017
Roger Jin <roger@lap-c02yq0hajhd2.wifi.local.cmu.edu>
2022-12-04 15:16:20 -0500

    train from checkpoint added

--- code/train_from_checkpoint.py
+++ code/train_from_checkpoint.py
@@ -7,14 +7,9 @@ import torch.optim as optim
 from warmup_scheduler import GradualWarmupScheduler # pip install git+https://github.com/ildoonet/pytorch-gradual-warmup-lr.git
 from loss import NT_xent_loss
 from torchsummary import summary
+from tqdm import tqdm
 
 args = get_args()
-
-CHECKPOINT = 'rocl_checkpoint/checkpoint200'
-checkpoint = torch.load(CHECKPOINT)
-epoch = checkpoint['epoch']
-model_state = checkpoint['model']
-optimizer_state = checkpoint['optimizer_state']
 
 #load data
 trainloader, traindst, testloader, testdst = data_loader.get_dataset(args)
@@ -28,6 +23,16 @@ else:
 model = ResNet18(10, contrastive_learning)
 summary(model.cuda(), (3, 32, 32))
 print('ResNet18 is loading ...')
+
+# load checkpoint
+CHECKPOINT = 'checkpoint/ckpt.t7training_1contrastive_ResNet18_cifar-10_b256_l256_0'
+checkpoint = torch.load(CHECKPOINT)
+start_epoch = checkpoint['epoch']
+
+model_state = checkpoint['model']
+model = model.load_state_dict(model_state)
+
+optimizer_state = checkpoint['optimizer_state']
 
 # to GPU
 if args.use_cuda:
@@ -47,5 +52,94 @@ base_optimizer  = optim.SGD(model_params, lr=args.lr, momentum=0.9, weight_decay
 optimizer = base_optimizer #LARS(optimizer=base_optimizer, eps=1e-8, trust_coef=0.001)
 
 # Cosine learning rate annealing (SGDR) & Learning rate warmup git from https://github.com/ildoonet/pytorch-gradual-warmup-lr #
-scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epoch)
+scheduler_cosine = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, args.epoch - start_epoch)
 scheduler_warmup = GradualWarmupScheduler(optimizer, multiplier=args.lr_multiplier, total_epoch=10, after_scheduler=scheduler_cosine)
+
+optimier = optimizer.load_state_dict(optimizer)
+
+import os
+os.makedirs('results', exist_ok=True) # make checkpoint folder
+
+args.name += (args.train_type + '_' +args.model + '_' + args.dataset + '_b' + str(args.batch_size)+'_l'+str(args.lamda))
+loginfo = 'results/log_' + args.name + '_' + str(args.seed)
+logname = (loginfo+ '.csv')
+print(loginfo)
+
+##### Log file #####
+import csv
+with open(logname, 'w') as logfile:
+    logwriter = csv.writer(logfile, delimiter=',')
+    logwriter.writerow(['epoch', 'train loss', 'reg loss'])
+
+print(args.name)
+
+def train(epoch):
+    model.train()
+    scheduler_warmup.step()
+
+    total_loss = 0
+    reg_simloss = 0
+    reg_loss = 0
+
+    for batch_idx, (ori, inputs_1, inputs_2, label) in tqdm(enumerate(trainloader)):
+        if args.use_cuda:
+            ori, inputs_1, inputs_2 = ori.cuda(), inputs_1.cuda() ,inputs_2.cuda() # to GPU
+        if args.attack_to=='original':
+            attack_target = inputs_1
+        else:
+            attack_target = inputs_2
+
+        if 'Rep' in args.advtrain_type :
+            advinputs = Rep.get_adversarial_example(imgs = inputs_1, target = attack_target)
+            adv_loss = Rep.get_adversarial_loss(imgs = inputs_1, target = attack_target, optimizer = optimizer) * 1.0 / args.lamda
+            reg_loss += adv_loss.data
+
+        if not (args.advtrain_type == 'None'):
+            inputs = torch.cat((inputs_1, inputs_2, advinputs))
+            slices = 3
+        else:
+            inputs = torch.cat((inputs_1, inputs_2))
+            slices = 2
+        
+        outputs = model(inputs)
+        # print(inputs, outputs)
+        # similarity, gathered_outputs = pairwise_similarity(outputs, temperature=args.temperature, multi_gpu=multi_gpu, adv_type = args.advtrain_type) 
+                
+        simloss  = NT_xent_loss(outputs, temperature=args.temperature, slices=slices) #, args.advtrain_type)
+        
+        if not (args.advtrain_type=='None'):
+            loss = simloss + adv_loss
+        else:
+            loss = simloss
+        
+        optimizer.zero_grad()
+        loss.backward()
+        total_loss += loss.data
+        reg_simloss += simloss.data
+        
+        optimizer.step()
+
+    return (total_loss/batch_idx, reg_simloss/batch_idx)
+
+##### Training #####
+for epoch in range(start_epoch, args.epoch):
+    train_loss, reg_loss = train(epoch)
+    
+    # Save checkpoint.
+    print('Saving..')
+    state = {
+        'epoch': epoch,
+        'model': model.state_dict(),
+        'optimizer_state' : optimizer.state_dict(),
+        'rng_state': torch.get_rng_state()
+    }
+
+    save_name = './checkpoint/ckpt.t7' + args.name + '_' + str(args.seed)
+
+    if not os.path.isdir('./checkpoint'):
+        os.mkdir('./checkpoint')
+    torch.save(state, save_name)
+    
+    with open(logname, 'a') as logfile:
+        logwriter = csv.writer(logfile, delimiter=',')
+        logwriter.writerow([epoch, train_loss.item(), reg_loss.item()])
