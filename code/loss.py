8adf9b2f4d38b3ec88cf05f6a023ca6779b3396e
Bhanu Prakash <bhanu.guda@yahoo.com>
2022-12-06 16:47:16 -0500

    added rince loss

--- code/loss.py
+++ code/loss.py
@@ -3,7 +3,7 @@ import torch
 epsilon = 1e-8
 
 
-def NT_xent_loss(outputs, temperature=1.0, slices=2):
+def NT_xent_loss(outputs, temperature=1.0, slices=2, rince=False):
     """
     Compute NT xent loss for output representations
 
@@ -38,13 +38,53 @@ def NT_xent_loss(outputs, temperature=1.0, slices=2):
     n = batch_size // slices
 
     # get loss of i-th item with i-th target and vice versa
-    NT_xent_loss = - torch.log(logits + epsilon)
-    losses = 0
+    if rince == False:
+        NT_xent_loss = - torch.log(logits + epsilon)
+        losses = 0
 
-    for i in range(slices):
-        for j in range(slices):
-            if i != j:
-                loss = NT_xent_loss[i * n: (i + 1) * n, j * n: (j + 1) * n].diag()
-                losses += loss.sum()
-    
-    return losses / batch_size
+        for i in range(slices):
+            for j in range(slices):
+                if i != j:
+                    loss = NT_xent_loss[i * n: (i + 1) * n, j * n: (j + 1) * n].diag()
+                    losses += loss.sum()
+        losses = losses / batch_size
+    else:
+        q = 0.1
+        lam = 0.01
+        pos = None
+        neg = None
+        # NT_xent_loss = - (logits) / q
+        losses = 0
+        
+        for i in range(slices):
+            row_pos = None
+            row_neg = None
+            for j in range(slices):
+                if i != j:
+                    sim_mat = logits[i * n: (i + 1) * n, j * n: (j + 1) * n]
+                    pos_mat = sim_mat.diag()
+                    neg_mat = torch.sum(sim_mat * (1 - torch.eye(sim_mat.shape[0], sim_mat.shape[1])).cuda(), 1)
+                    if row_pos is None:
+                        row_pos = pos_mat
+                    else:
+                        row_pos += pos_mat
+                    if row_neg is None:
+                        row_neg = neg_mat
+                    else:
+                        row_neg += neg_mat
+                    # pos += sim_mat.diag()
+                    # neg += torch.sum()
+                    # loss = NT_xent_loss[i * n: (i + 1) * n, j * n: (j + 1) * n].diag()
+                    # losses += loss.sum()
+            if pos is None:
+                pos = row_pos
+            else:
+                pos = torch.cat((pos, row_pos))
+            if neg is None:
+                neg = row_neg
+            else:
+                neg = torch.cat((neg, row_neg))
+        neg = ((lam*(pos + neg))**q) / q
+        pos = -(pos**q) / q
+        losses = pos.mean() + neg.mean()
+    return losses
