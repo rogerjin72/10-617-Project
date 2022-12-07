1e6176fb9ffc9cf63992585dac5752eebbe1f575
Roger Jin <roger@lap-c02yq0hajhd2.wifi.local.cmu.edu>
2022-12-07 15:36:51 -0500

    refactor

--- RoCL-master/src/test_linear.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python linear_eval.py --ngpu 1 --batch-size=1024 \
-      --train_type='linear_eval' --model=ResNet18 --epoch 150 --lr 0.1 --name training_1 \
-      --load_checkpoint=None \
-      --clean=True --dataset=cifar-10 --seed=0 
--- RoCL-master/src/train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python rocl_train.py --ngpu 1 \
-                    --batch-size=256 --model='ResNet18' --k=7 --loss_type='sim' --advtrain_type='Rep' \
-                    --attack_type='linf' --name='authors' --regularize_to='other' --attack_to='other' \
-                    --train_type='contrastive' --dataset='cifar-10' --finetune
